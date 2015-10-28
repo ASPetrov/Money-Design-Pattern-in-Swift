fb7c46071b8fbc25a4bb3ceb0731257a3c822eff
Aleksandar Petrov <aleksandar.petrov@mentormate.com>
2015-10-28 11:36:46 +0200

    Create Framework projects and workspace

---
+++ TestFrameworkApp/TestFrameworkApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFrameworkApp
+//
+//  Created by Aleksandar Petrov on 10/28/15.
+//  Copyright © 2015 Aleksandar Petrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
